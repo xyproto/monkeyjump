9c3f3aede38ce587238d9ae66d7232fb8c654b39
Alexander Rødseth <alexander.rodseth@usit.uio.no>
2013-08-02 14:20:00 +0200

    Added the beginnings of a setuptools file, needs more work

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "monkeyjump",
+    version = "0.5",
+    author = "Alexander Rødseth",
+    author_email = "rodseth@gmail.com",
+    description = ("Minimalistic GUI with keybindings for GnuGo and other GTP applications."),
+    license = "GPL2",
+    keywords = "go gnugo pygame python game gtp board",
+    url = "http://github.com/xyproto/monkeyjump",
+    packages=['burnlib'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Games",
+        "License :: OSI Approved :: GPL 2",
+    ],
+)
